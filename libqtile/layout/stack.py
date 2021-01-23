f679d3994ad83b08b91ae01cd849cf1b3b93d053
elParaguayo <elparaguayocode@gmail.com>
2021-01-23 20:05:13 +0000

    Playing around with stack layout

--- libqtile/layout/stack.py
+++ libqtile/layout/stack.py
@@ -17,8 +17,204 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import cairocffi
+
 from libqtile import utils
 from libqtile.layout.base import Layout, _ClientList
+from libqtile.log_utils import logger
+
+from xcffib.xproto import StackMode
+
+from libqtile import configurable, drawer, pangocffi, window
+
+
+# This is basically just libqtile/popup.py with the textlayout taken out and
+# some extra functions to draw icons
+
+class StackPreview(configurable.Configurable):
+    """
+    This class can be used to create popup windows that display images and/or text.
+    """
+    defaults = [
+        ('opacity', 1.0, 'Opacity of notifications.'),
+        ('foreground', '#ffffff', 'Colour of text.'),
+        ('background', '#111111', 'Background colour.'),
+        ('border', '#111111', 'Border colour.'),
+        ('border_width', 0, 'Line width of drawn borders.'),
+        ('corner_radius', None, 'Corner radius for round corners, or None.'),
+        ('font', 'sans', 'Font used in notifications.'),
+        ('font_size', 14, 'Size of font.'),
+        ('fontshadow', None, 'Colour for text shadows, or None for no shadows.'),
+        ('horizontal_padding', 0, 'Padding at sides of text.'),
+        ('vertical_padding', 0, 'Padding at top and bottom of text.'),
+        ('margin', None, 'Space around window as int or list of ints [N E S W]'),
+        ('text_alignment', 'left', 'Text alignment: left, center or right.'),
+        ('wrap', True, 'Whether to wrap text.'),
+    ]
+
+    _icons_cache = {}
+
+    def __init__(self, qtile, x=50, y=50, width=256, height=64, **config):
+        configurable.Configurable.__init__(self, **config)
+        self.add_defaults(StackPreview.defaults)
+        self.qtile = qtile
+
+        win = qtile.conn.create_window(x, y, width, height)
+        win.set_property("QTILE_INTERNAL", 1)
+        self.win = window.Internal(win, qtile)
+        self.qtile.windows_map[self.win.window.wid] = self.win
+
+        self.drawer = drawer.Drawer(
+            self.qtile, self.win.window.wid, width, height,
+        )
+
+        if self.border_width:
+            self.win.window.configure(borderwidth=self.border_width)
+        if self.corner_radius:
+            self.win.window.round_corners(width, height, self.corner_radius, self.border_width)
+
+        self.win.handle_Expose = self._handle_Expose
+        self.win.handle_KeyPress = self._handle_KeyPress
+        self.win.handle_ButtonPress = self._handle_ButtonPress
+
+        self.x = self.win.x
+        self.y = self.win.y
+        if not self.border_width:
+            self.border = None
+
+        self.orig_width = width
+
+    def _handle_Expose(self, e):  # noqa: N802
+        pass
+
+    def _handle_KeyPress(self, event):  # noqa: N802
+        pass
+
+    def _handle_ButtonPress(self, event):  # noqa: N802
+        if event.detail == 1:
+            self.hide()
+
+    @property
+    def width(self):
+        return self.win.width
+
+    @width.setter
+    def width(self, value):
+        self.win.width = value
+        self.drawer.width = value
+
+    @property
+    def height(self):
+        return self.win.height
+
+    @height.setter
+    def height(self, value):
+        self.win.height = value
+        self.drawer.height = value
+
+    def set_border(self, color):
+        self.win.window.paint_borders(color)
+
+    def clear(self):
+        self.drawer.clear(self.background)
+
+    def draw(self):
+        self.drawer.draw()
+
+    def place(self):
+        self.win.place(
+            self.x, self.y, self.width, self.height,
+            self.border_width, self.border, above=False, margin=self.margin
+        )
+
+    def unhide(self):
+        self.win.unhide()
+        self.win.window.configure(stackmode=StackMode.Below)
+
+    def hide(self):
+        self.win.hide()
+
+    def kill(self):
+        self.win.kill()
+
+    def show_stack_icons(self, stack, current_win):
+        self.stack = stack
+
+        if not stack:
+            logger.warning("Stack empty")
+            clist.hide()
+            return
+
+        self.width = self.orig_width
+        self.height = len(stack) * self.width
+
+        offset = 0
+
+        self.drawer.clear("000000")
+
+        for client in stack:
+            icon = self.get_window_icon(client)
+            self.draw_icon(icon, offset)
+            offset += self.width
+
+        if current_win in stack:
+            id = stack.index(current_win)
+            self.drawer.set_source_rgb("00FFFF")
+            self.drawer.rectangle(0, id * self.width, self.width, self.width)
+
+        self.place()
+        self.draw()
+        self.unhide()
+
+    def draw_icon(self, surface, offset):
+        if not surface:
+            return
+
+        x = 2
+        y = offset
+
+        self.drawer.ctx.save()
+        self.drawer.ctx.translate(x, y)
+        self.drawer.ctx.set_source(surface)
+        self.drawer.ctx.paint()
+        self.drawer.ctx.restore()
+
+    def get_window_icon(self, window):
+        if not window.icons:
+            return None
+
+        self.icon_size = self.width - 4
+
+        cache = self._icons_cache.get(window.window.wid)
+        if cache:
+            return cache
+
+        icons = sorted(
+            iter(window.icons.items()),
+            key=lambda x: abs(self.icon_size - int(x[0].split("x")[0]))
+        )
+        icon = icons[0]
+        width, height = map(int, icon[0].split("x"))
+
+        img = cairocffi.ImageSurface.create_for_data(
+            icon[1],
+            cairocffi.FORMAT_ARGB32,
+            width,
+            height
+        )
+
+        surface = cairocffi.SurfacePattern(img)
+
+        scaler = cairocffi.Matrix()
+
+        if height != self.icon_size:
+            sp = height / self.icon_size
+            height = self.icon_size
+            width /= sp
+            scaler.scale(sp, sp)
+        surface.set_matrix(scaler)
+        self._icons_cache[window.window.wid] = surface
+        return surface
 
 
 class _WinStack(_ClientList):
@@ -63,6 +259,7 @@ class Stack(Layout):
         ("num_stacks", 2, "Number of stacks."),
         ("fair", False, "Add new windows to the stacks in a round robin way."),
         ("margin", 0, "Margin of the layout (int or list of ints [N E S W])"),
+        ("preview_width", 30, "Size of preview pane")
     ]
 
     def __init__(self, **config):
@@ -70,6 +267,9 @@ class Stack(Layout):
         self.add_defaults(Stack.defaults)
         self.stacks = [_WinStack(autosplit=self.autosplit)
                        for i in range(self.num_stacks)]
+
+        self.qtile = None
+        self.previews = {}
 
     @property
     def current_stack(self):
@@ -191,9 +391,12 @@ class Stack(Layout):
 
     def remove(self, client):
         current_offset = self.current_stack_offset
-        for i in self.stacks:
-            if client in i:
-                i.remove(client)
+        for i, stack in enumerate(self.stacks):
+            if client in stack:
+                stack.remove(client)
+                preview = self.previews.get(i)
+                if preview:
+                    preview.hide()
                 break
         if self.stacks[current_offset].cw:
             return self.stacks[current_offset].cw
@@ -206,6 +409,10 @@ class Stack(Layout):
                 return n.cw
 
     def configure(self, client, screen_rect):
+        if not self.qtile:
+            from libqtile import qtile
+            self.qtile = qtile
+
         for i, s in enumerate(self.stacks):
             if client in s:
                 break
@@ -220,7 +427,7 @@ class Stack(Layout):
 
         column_width = int(screen_rect.width / len(self.stacks))
         xoffset = screen_rect.x + i * column_width
-        window_width = column_width - 2 * self.border_width
+        window_width = column_width - 2 * self.border_width - self.preview_width
 
         if s.split:
             column_height = int(screen_rect.height / len(s))
@@ -239,7 +446,7 @@ class Stack(Layout):
         else:
             if client == s.cw:
                 client.place(
-                    xoffset,
+                    xoffset + self.preview_width,
                     screen_rect.y,
                     window_width,
                     screen_rect.height - 2 * self.border_width,
@@ -250,6 +457,21 @@ class Stack(Layout):
                 client.unhide()
             else:
                 client.hide()
+
+            if self.qtile:
+                preview = self.previews.get(i)
+                if not preview:
+                    preview = StackPreview(self.qtile,
+                                           xoffset,
+                                           screen_rect.y,
+                                           self.preview_width,
+                                           screen_rect.height,
+                                           margin=self.margin
+                                           )
+                    self.previews[i] = preview
+
+            if preview:
+                preview.show_stack_icons(self.stacks[i], s.cw)
 
     def info(self):
         d = Layout.info(self)
